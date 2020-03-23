2e5572986feec013cb8767eb907c4c5da234213c
TasneemMohammad <tasneemmohamed504@gmail.com>
2020-03-23 23:39:53 +0200

    added_database5

--- app/src/main/java/com/example/l/MyDataBase.java
+++ app/src/main/java/com/example/l/MyDataBase.java
@@ -9,7 +9,7 @@ import androidx.annotation.Nullable;
 
 public class MyDataBase extends SQLiteOpenHelper {
     public MyDataBase(@Nullable Context context) {
-        super(context,"car", null,2);
+        super(context,"car", null,3);
     }
 
     @Override
