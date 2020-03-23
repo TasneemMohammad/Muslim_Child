eb4cee2631f72d4c42b17c5f8167c1721a1fd547
TasneemMohammad <tasneemmohamed504@gmail.com>
2020-03-23 23:26:42 +0200

    added_database

--- app/src/main/java/com/example/l/MyDataBase.java
+++ app/src/main/java/com/example/l/MyDataBase.java
@@ -3,6 +3,7 @@ package com.example.l;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
